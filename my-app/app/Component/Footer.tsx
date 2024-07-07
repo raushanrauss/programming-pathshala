c9bbe653c8c333b16b21dfddc31bf3222860ae08
John Doe <john@example.com>
2024-07-07 21:55:13 +0530

    adjusted footer part

--- my-app/app/Component/Footer.tsx
+++ my-app/app/Component/Footer.tsx
@@ -37,52 +37,55 @@ export const footerLinks = [
 
 const FooterComponent = () => {
     return (
-        <Container className="flex flex-col lg:flex-row justify-between items-center lg:items-start px-5 py-5 bg-slate-600 lg:w-auto"
-            data-aos="fade-up">
-            <div className="text-white w-full lg:w-auto">
-                <Link href="/" className="flex items-center gap-2">
-                    <Image src="/LogoIcon.png" width={35} height={24} alt="logo" />
-                    <span className="my-auto text-2xl font-bold">Nexcent</span>
-                </Link>
-                <div className="mt-4 text-silver text-sm">
-                    <p>Copyright © 2020 Nexcent ltd.</p>
-                    <p className="mt-1">All rights reserved</p>
+        <div className="bg-slate-600">
+            <Container className="flex flex-col lg:flex-row justify-between items-center lg:items-start px-5 py-5 bg-slate-600 lg:w-auto"
+                data-aos="fade-up">
+                <div className="text-white w-full lg:w-auto">
+                    <Link href="/" className="flex items-center gap-2">
+                        <Image src="/LogoIcon.png" width={35} height={24} alt="logo" />
+                        <span className="my-auto text-2xl font-bold">Nexcent</span>
+                    </Link>
+                    <div className="mt-4 text-silver text-sm">
+                        <p>Copyright © 2020 Nexcent ltd.</p>
+                        <p className="mt-1">All rights reserved</p>
+                    </div>
+                    <div className="mt-5 flex gap-3">
+                        {socialMediaContent.map((item, index) => (
+                            <SocialIcon key={index} imgUrl={item} />
+                        ))}
+                    </div>
                 </div>
-                <div className="mt-5 flex gap-3">
-                    {socialMediaContent.map((item, index) => (
-                        <SocialIcon key={index} imgUrl={item} />
+                <div className="flex flex-col lg:flex-row gap-5 w-full lg:w-auto">
+                    {footerLinks.map((items, index) => (
+                        <div key={index} className="text-white">
+                            <h5>{items.title}</h5>
+                            <ul className="flex flex-col gap-2">
+                                {items.links.map((link, idx) => (
+                                    <li key={idx}>
+                                        <span>{link}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     ))}
-                </div>
-            </div>
-            <div className="flex flex-col lg:flex-row gap-5 w-full lg:w-auto">
-                {footerLinks.map((items, index) => (
-                    <div key={index} className="text-white">
-                        <h5>{items.title}</h5>
-                        <ul className="flex flex-col gap-2">
-                            {items.links.map((link, idx) => (
-                                <li key={idx}>
-                                    <span>{link}</span>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-                <div className="flex flex-col gap-2">
-                    <h5 className="text-white">Stay up to date</h5>
-                    <div className="flex">
-                        <input
-                            type="email"
-                            className="form-control bg-dark text-white shadow-none border-0"
-                            id="exampleFormControlInput1"
-                            placeholder="Your email address"
-                        />
-                        <button className="btn btn-dark">
-                            <Image src="/inputIcon.png" height={20} width={20} alt="send" />
-                        </button>
+                    <div className="flex flex-col gap-2">
+                        <h5 className="text-white">Stay up to date</h5>
+                        <div className="flex">
+                            <input
+                                type="email"
+                                className="form-control bg-dark text-white shadow-none border-0"
+                                id="exampleFormControlInput1"
+                                placeholder="Your email address"
+                            />
+                            <button className="btn btn-dark">
+                                <Image src="/inputIcon.png" height={20} width={20} alt="send" />
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </div>
+        
     );
 };
 
